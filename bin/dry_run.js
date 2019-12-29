cebd9d25ce0dca780a056247edf603e7757a401e
brendanbernas <brendan.bernas@gmail.com>
2019-12-29 17:23:36 -0500

    Add basic scraping functionality

--- bin/dry_run.js
+++ bin/dry_run.js
@@ -1,12 +1,35 @@
 const JobbankQueryBuilder = require("../lib/query/jobbank_query_builder");
 const crawl = require("../lib/crawl/jobbank_search_crawler");
+const scrape = require("../lib/scrape/jobbank_scraper");
+const fs = require('fs');
 
 jbUrl = new JobbankQueryBuilder().location('Alberta').term('designer').build();
 
+function log_err(err){
+  console.log("Error: ", err);
+}
+
 crawl(jbUrl)
 .then(function(urls){
-  console.log("Found urls: ", urls);
+  console.log(`Found ${urls.length} urls.`);
+
+  let found = []
+  for(i in urls){
+    let url = urls[i];
+    console.log(`Scraping ${url}`);
+    scrape(url)
+    .then(
+      function(res){
+        found.push(res);
+        // console.log(res);
+      }
+    ).catch(log_err);
+  }
+  // FIXME ensure all scraping promises are fulfilled to write results
+  console.log(found);
+  fs.writeFile('results.json', found, 'utf8', function(res){
+    console.log(res);
+  });
+
 })
-.catch(function (error){
-  console.log("Error: ",  error);
-});
+.catch(log_err);
